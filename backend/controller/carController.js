afac2acf9420c1a8a60430d6a7e9845f038a9975
1804rbt <fabricerob@yahoo.fr>
2024-08-23 02:43:40 +0200

    middleware, payments pages, routes and stuffs

--- backend/controller/carController.js
+++ backend/controller/carController.js
@@ -7,9 +7,7 @@ exports.getCarAll = async (req, res) => {
     console.log("query running....");
     const rows = await connect.pool.query("SELECT * FROM model_car");
     console.log("query running....");
-    const rows = await connect.pool.query(
-      "SELECT c.id, c.serieCode,mc.nameModel,mc.picture,mc.doors,mc.dimension,mc.place,mc.basePrice FROM car c JOIN model_car mc ON c.IdModel = mc.id"
-    );
+
     res.status(200).json(rows);
   } catch (error) {
     console.log("erreur", error);
